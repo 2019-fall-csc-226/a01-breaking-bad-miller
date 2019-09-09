e9e938af65a2a03fb306eac984afa1806e3892e5
millerd2 <millerd2@berea.edu>
2019-09-08 20:46:36 -0400

    Finished the homework

--- a01_stubs.py
+++ a01_stubs.py
@@ -1,6 +1,6 @@
 ######################################################################
-# Author: Dr. Scott Heggen      TODO: Change this to your name
-# Username: heggens             TODO: Change this to your username
+# Author: Dakota Miller      TODO: Change this to your name
+# Username: millerd2            TODO: Change this to your username
 #
 # Assignment: A01
 #
@@ -17,8 +17,35 @@
 ######################################################################
 # (Required) Task 1
 # TODO Ask user for their birth year
+birth_year = int(input("What is your birth year"))
+print(birth_year)
 
-
+if birth_year == int(1999):
+    print("You are a rabbit, You must be fast.")
+if birth_year == int(2000):
+     print("Wow you are a DRAGON!")
+if birth_year == int(2001):
+    print("You are a snake, scary.")
+if birth_year == int(2002):
+    print("You are a horse, do you trot?")
+if birth_year == int(2003):
+    print("What a Goat")
+if birth_year == int(2004):
+    print("You are a monkey, do you like monkeying around?")
+if birth_year == int(2005):
+    print("You arr a rooster, are you a early riser?")
+if birth_year == int(2006):
+    print("You are a dog, woof.")
+if birth_year == int(2007):
+    print("You are a pig, oink oink.")
+if birth_year == int(2008):
+    print("You are a rat, eww.")
+if birth_year == int(2009):
+    print("You are an ox, are you strong?")
+if birth_year == int(2010):
+    print("You are a tiger, that's cool.")
+if birth_year == int(2011):
+    print("You are a rabbit, are you fast?")
 # TODO Check the year using if conditionals, and print the correct animal for that year.
 # See the a01_pets.py for examples
 
@@ -26,6 +53,11 @@
 ######################################################################
 # (Required) Task 2
 # TODO Ask the user for their friend's birth year
+buddies = int(input("Hey buddy what is your birth year?"))
+print(buddies)
+if buddies == int(2000):
+    print("You are a dragon, thats crazy dude!")
+else: print(" You aren't my friend.")
 
 
 # TODO Similar to above, check your friend's year using if conditionals, and print the correct animal for that year
